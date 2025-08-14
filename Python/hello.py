3880f523d6a4adc81fe5328099968df6a22c978e
SophiaHu-194412 <194412@cloud.rangitoto.school.nz>
2025-08-15 10:48:23 +1200

    Update hello.py

--- Python/hello.py
+++ Python/hello.py
@@ -1,4 +1,9 @@
-import tkinter
-m = tkinter.Tk()
-
-m.mainloop()
+from tkinter import *
+master = Tk()
+Label(master, text="Hello World!").pack()
+Label(master, text="This is a simple GUI application.").pack()
+e1 = Entry(master)
+e2 = Entry(master)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+mainloop()
